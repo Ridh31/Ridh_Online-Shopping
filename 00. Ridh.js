d77fa17e126f1e9bb8fc8ae780cbc9a0f842495f
Ridh31 <82384108+Ridh31@users.noreply.github.com>
2022-01-12 09:00:59 +0700

    Add files via upload
    File

---
+++ 00. Ridh.js
@@ -0,0 +1,4 @@
+function click() {
+  var email = document.getElementById("exampleInputEmail1").value;
+  return confirm("You have subscribe our plan!" + "\n" + "We have sent our special subscription through this email: " + email);
+}
